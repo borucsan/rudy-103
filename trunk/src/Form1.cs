95db9e52ead2cb575225a7605858ab6c382aef88
dawidkarwot <dawidkarwot@7691d683-f540-25e5-aa27-d06a4871c7f6>
2012-03-12 09:42:26 +0000

    Podstawowy szkielet projektu.

---
+++ trunk/src/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Rudy_103
+{
+    public partial class MainWindow : Form
+    {
+        /// <summary>
+        /// Konstruktor klasy głównego okna.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
