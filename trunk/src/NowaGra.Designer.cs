fe2d4fdb5f7ff5d182cd1dc8c9006296130b9644
adamus1115@gmail.com <adamus1115@gmail.com@7691d683-f540-25e5-aa27-d06a4871c7f6>
2012-03-24 10:49:46 +0000

    Dodałem dwie puste klasy Gracz.cs oraz Przeciwnik.cs, dużo zmian w NowaGra.cs Dodałem paski energii, na spacji zmniejszamy energie, jeszcze walcze z tym żeby obraz nie migał, próbowałem zrobić podwójne buforowanie, ale nie dało to efektu. Będę potrzebował klase plansza oraz dwuwymiarową tablicę. Widać w kodzie, że są potrzebne kontenery danych, parę rzeczy zostało zakomentowane, które później trzeba będzie użyć do np. przęgladania pól i rysowania obiektów.

--- trunk/src/NowaGra.Designer.cs
+++ trunk/src/NowaGra.Designer.cs
@@ -28,90 +28,175 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NowaGra));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.energialabel = new System.Windows.Forms.Label();
-            this.przeciwniklabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.energiapicture = new System.Windows.Forms.PictureBox();
+            this.poziompanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.czaspanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.czaslabel = new System.Windows.Forms.Label();
+            this.mapapanel = new System.Windows.Forms.Panel();
+            this.minimapapictureBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer();
+            this.czas_rozgrywki = new System.Windows.Forms.Timer();
             this.panel1.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.poziompanel.SuspendLayout();
+            this.czaspanel.SuspendLayout();
+            this.mapapanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.energiapicture);
+            this.panel1.Controls.Add(this.poziompanel);
+            this.panel1.Controls.Add(this.czaspanel);
+            this.panel1.Controls.Add(this.mapapanel);
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(240, 294);
+            this.panel1.Size = new System.Drawing.Size(240, 320);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.label4.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(122, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 20);
+            this.label4.Text = "Energia";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // energiapicture
+            // 
+            this.energiapicture.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.energiapicture.Location = new System.Drawing.Point(122, 0);
+            this.energiapicture.Name = "energiapicture";
+            this.energiapicture.Size = new System.Drawing.Size(57, 60);
+            this.energiapicture.Paint += new System.Windows.Forms.PaintEventHandler(this.energiapicture_Paint);
+            // 
+            // poziompanel
+            // 
+            this.poziompanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.poziompanel.Controls.Add(this.label2);
+            this.poziompanel.Controls.Add(this.label1);
+            this.poziompanel.Location = new System.Drawing.Point(0, 0);
+            this.poziompanel.Name = "poziompanel";
+            this.poziompanel.Size = new System.Drawing.Size(57, 60);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Tahoma", 16F, System.Drawing.FontStyle.Regular);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 33);
+            this.label2.Text = "1";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 24);
+            this.label1.Text = "Poziom";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // czaspanel
+            // 
+            this.czaspanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.czaspanel.Controls.Add(this.label3);
+            this.czaspanel.Controls.Add(this.czaslabel);
+            this.czaspanel.Location = new System.Drawing.Point(61, 0);
+            this.czaspanel.Name = "czaspanel";
+            this.czaspanel.Size = new System.Drawing.Size(57, 60);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Regular);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.Text = "Czas";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // czaslabel
+            // 
+            this.czaslabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.czaslabel.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular);
+            this.czaslabel.ForeColor = System.Drawing.Color.White;
+            this.czaslabel.Location = new System.Drawing.Point(4, 24);
+            this.czaslabel.Name = "czaslabel";
+            this.czaslabel.Size = new System.Drawing.Size(51, 22);
+            this.czaslabel.Text = "00:00";
+            this.czaslabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // mapapanel
+            // 
+            this.mapapanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.mapapanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.mapapanel.Controls.Add(this.minimapapictureBox);
+            this.mapapanel.Location = new System.Drawing.Point(183, 0);
+            this.mapapanel.Name = "mapapanel";
+            this.mapapanel.Size = new System.Drawing.Size(57, 60);
+            // 
+            // minimapapictureBox
+            // 
+            this.minimapapictureBox.BackColor = System.Drawing.Color.White;
+            this.minimapapictureBox.Location = new System.Drawing.Point(4, 7);
+            this.minimapapictureBox.Name = "minimapapictureBox";
+            this.minimapapictureBox.Size = new System.Drawing.Size(50, 50);
+            this.minimapapictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.minimapapictureBox_Paint);
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 44);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 60);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(240, 247);
+            this.pictureBox1.Size = new System.Drawing.Size(240, 260);
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.panel2.Controls.Add(this.energialabel);
-            this.panel2.Controls.Add(this.przeciwniklabel);
-            this.panel2.Controls.Add(this.label1);
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(240, 41);
-            // 
-            // energialabel
-            // 
-            this.energialabel.Location = new System.Drawing.Point(137, 1);
-            this.energialabel.Name = "energialabel";
-            this.energialabel.Size = new System.Drawing.Size(100, 20);
-            this.energialabel.Text = "Energia: 3";
-            this.energialabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // przeciwniklabel
-            // 
-            this.przeciwniklabel.Location = new System.Drawing.Point(3, 1);
-            this.przeciwniklabel.Name = "przeciwniklabel";
-            this.przeciwniklabel.Size = new System.Drawing.Size(128, 20);
-            this.przeciwniklabel.Text = "Przeciwników: 20";
-            this.przeciwniklabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // label1
-            // 
-            this.label1.Location = new System.Drawing.Point(3, 21);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(234, 20);
-            this.label1.Text = "Punkty: 0";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // timer1
             // 
             this.timer1.Interval = 30;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // czas_rozgrywki
+            // 
+            this.czas_rozgrywki.Interval = 1000;
+            this.czas_rozgrywki.Tick += new System.EventHandler(this.czas_rozgrywki_Tick);
+            // 
             // NowaGra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(240, 294);
+            this.ClientSize = new System.Drawing.Size(240, 320);
             this.ControlBox = false;
             this.Controls.Add(this.panel1);
             this.KeyPreview = true;
+            this.Location = new System.Drawing.Point(0, 0);
             this.MinimizeBox = false;
             this.Name = "NowaGra";
             this.Text = "Rudy 103 - Nowa Gra";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.NowaGra_KeyUp);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.NowaGra_KeyDown);
             this.panel1.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
+            this.poziompanel.ResumeLayout(false);
+            this.czaspanel.ResumeLayout(false);
+            this.mapapanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -121,9 +206,16 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Label przeciwniklabel;
+        private System.Windows.Forms.Panel mapapanel;
+        private System.Windows.Forms.PictureBox minimapapictureBox;
+        private System.Windows.Forms.Timer czas_rozgrywki;
+        private System.Windows.Forms.Panel czaspanel;
+        private System.Windows.Forms.Label czaslabel;
+        private System.Windows.Forms.Panel poziompanel;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label energialabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox energiapicture;
+        private System.Windows.Forms.Label label4;
     }
 }
---
+++ trunk/src/Przeciwnik.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rudy_103.src
+{
+    class Przeciwnik
+    {
+    }
+}
