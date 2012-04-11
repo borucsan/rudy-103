bb674c284289b7d9386e5b75b2968859f837baf6
dawidkarwot <dawidkarwot@7691d683-f540-25e5-aa27-d06a4871c7f6>
2012-04-11 09:56:27 +0000

    Zapomniałem wysłać pliki drzew.

---
+++ src/ElementDrzewa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Rudy_103.src
+{
+    class ElementDrzewa<T> where T : Obiekty
+    {
+        public List<T> lista { get; set; }
+        public Rectangle Wymiary { get; set; }
+        public ElementDrzewa<T> lewo { get; set; }
+        public ElementDrzewa<T> prawo { get; set; }
+        public int iteracja;
+        public ElementDrzewa(List<T> sublist, Rectangle wym)
+        {
+            lista = sublist;
+            Wymiary = wym;
+            prawo = lewo = null;
+        }
+        public ElementDrzewa(List<T> sublist, Rectangle wym, int iteracja)
+        {
+            lista = sublist;
+            Wymiary = wym;
+            prawo = lewo = null;
+            this.iteracja = iteracja;
+        }
+    }
+}
