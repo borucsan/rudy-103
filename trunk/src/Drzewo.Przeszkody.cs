f7a54dddcfc69d91c0f5caf4013dd78a0cab1d7c
dawidkarwot <dawidkarwot@7691d683-f540-25e5-aa27-d06a4871c7f6>
2012-04-11 09:56:27 +0000

    Zapomniałem wysłać pliki drzew.

---
+++ trunk/src/Drzewo.Przeszkody.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Rudy_103.src
+{
+    /// <summary>
+    /// Klasa pochodna po drzewie BSP do podziału przeszkód na obszary.
+    /// </summary>
+    class DrzewoPrzeszkody : Drzewo<Przeszkoda>
+    {
+        /// <summary>
+        /// Konstruktor drzewa BSP dla przeszkód.
+        /// </summary>
+        /// <param name="lista_obiektow">Lista wszystkich obiektów podlegających podziałowi</param>
+        /// <param name="wymiar">Prostokąt obiektu podlegającego partycjowaniu</param>
+        /// <param name="glebokosc">Określa ile ma być węzłów w drzewie.</param>
+        /// <param name="kompresja">Parametr umożliwiający tworzenie drzewa zkompresowanego(Zawierającego tylko listy w liściach.</param>
+        public DrzewoPrzeszkody(List<Przeszkoda> lista_obiektow, Rectangle wymiar, int glebokosc, bool kompresja) : 
+            base(lista_obiektow, wymiar, glebokosc, kompresja) { }
+        /// <summary>
+        /// Metoda sprawdzająca kolizję z podanym obiektem.
+        /// </summary>
+        /// <param name="obj">Objekt z którym przetestować kolizję</param>
+        /// <returns>Zwraca true jeśli wykryje kolizję z dowolną przeszkodą</returns>
+        public bool CzyKoliduje(Obiekty obj)
+        {
+            if (obj.wymiary.IntersectsWith(root.lewo.Wymiary)) if (Koliduje(root.lewo, obj.wymiary)) return true;
+            if (obj.wymiary.IntersectsWith(root.prawo.Wymiary)) if (Koliduje(root.prawo, obj.wymiary)) return true;
+            return false;
+        }
+        /// <summary>
+        /// Metoda sprawdzająca kolizję z podanym prostokątem.
+        /// </summary>
+        /// <param name="rec">Prostokąt z którym przetestować kolizję</param>
+        /// <returns>Zwraca true jeśli wykryje kolizję z dowolną przeszkodą</returns>
+        public bool CzyKoliduje(Rectangle rec)
+        {
+            if (rec.IntersectsWith(root.lewo.Wymiary)) if(Koliduje(root.lewo, rec)) return true;
+            if (rec.IntersectsWith(root.prawo.Wymiary)) if(Koliduje(root.prawo, rec)) return true;
+            return false;
+        }
+        /// <summary>
+        /// Metoda sprawdzająca kolizję z podanym pociskiem.
+        /// </summary>
+        /// <param name="poc">Pocisk z którym przetestować kolizję</param>
+        /// <returns>Zwraca true jeśli wykryje kolizję z dowolną przeszkodą</returns>
+        public bool CzyKoliduje(Pocisk poc)
+        {
+            if (poc.wymiary.IntersectsWith(root.lewo.Wymiary)) if(Koliduje(root.lewo, poc)) return true;
+            if (poc.wymiary.IntersectsWith(root.prawo.Wymiary)) if(Koliduje(root.prawo, poc)) return true;
+            return false;
+        }
+        /// <summary>
+        /// Metoda wewnętrzna wspomagająca sprawdzanie kolizji
+        /// </summary>
+        /// <param name="iterator">Kolejny węzeł drzewa</param>
+        /// <param name="poc">Pocisk z którym przetestować kolizję</param>
+        /// <returns>Zwraca true jeśli wykryje kolizję z dowolną przeszkodą</returns>
+        private bool Koliduje(ElementDrzewa<Przeszkoda> iterator, Pocisk poc)
+        {
+            if (iterator.lewo != null || iterator.prawo != null)
+            {
+                if (iterator.lewo != null)
+                {
+                    if (poc.wymiary.IntersectsWith(iterator.lewo.Wymiary))
+                    {
+                        if (Koliduje(iterator.lewo, poc)) return true;
+                    }
+                }
+                if (iterator.prawo != null)
+                {
+                    if (poc.wymiary.IntersectsWith(iterator.prawo.Wymiary))
+                    {
+                        if (Koliduje(iterator.prawo, poc)) return true;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < iterator.lista.Count; ++i)
+                {
+                    if (iterator.lista[i].transparent) continue;
+                    if (poc.wymiary.IntersectsWith(iterator.lista[i].wymiary))
+                    {
+                        if (iterator.lista[i].Uszkodz(poc.sila))
+                        {
+                            iterator.lista.RemoveAt(i);
+                        }
+                        return true;
+                    }
+
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// Metoda wewnętrzna wspomagająca sprawdzanie kolizji
+        /// </summary>
+        /// <param name="iterator">Kolejny węzeł drzewa</param>
+        /// <param name="rec">Prostokąt z którym przetestować kolizję</param>
+        /// <returns>Zwraca true jeśli wykryje kolizję z dowolną przeszkodą</returns>
+        private bool Koliduje(ElementDrzewa<Przeszkoda> iterator, Rectangle rec)
+        {
+            if (iterator.lewo != null || iterator.prawo != null)
+            {
+                if (iterator.lewo != null)
+                {
+                    if (rec.IntersectsWith(iterator.lewo.Wymiary))
+                    {
+                        if (Koliduje(iterator.lewo, rec)) return true;
+                    }
+                }
+                if (iterator.prawo != null)
+                {
+                    if (rec.IntersectsWith(iterator.prawo.Wymiary))
+                    {
+                        if (Koliduje(iterator.prawo, rec)) return true;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < iterator.lista.Count; ++i)
+                {
+                    if (iterator.lista[i].transparent) continue;
+                    if (rec.IntersectsWith(iterator.lista[i].wymiary)) return true;
+                }
+            }
+            return false;
+        }
+        public void RysujElementy(Rectangle rec, Graphics g, System.Drawing.Imaging.ImageAttributes transparentPink)
+        {
+            if (rec.IntersectsWith(root.lewo.Wymiary)) RysujEle(root.lewo, rec, g, transparentPink);
+            if (rec.IntersectsWith(root.prawo.Wymiary)) RysujEle(root.prawo, rec, g, transparentPink);
+        }
+        private void RysujEle(ElementDrzewa<Przeszkoda> iterator, Rectangle rec, Graphics g, System.Drawing.Imaging.ImageAttributes transparentPink)
+        {
+            if (iterator.lewo != null || iterator.prawo != null)
+            {
+                if (iterator.lewo != null)
+                {
+                    if (rec.IntersectsWith(iterator.lewo.Wymiary))
+                    {
+                        RysujEle(iterator.lewo, rec, g, transparentPink);
+                    }
+                }
+                if (iterator.prawo != null)
+                {
+                    if (rec.IntersectsWith(iterator.prawo.Wymiary))
+                    {
+                        RysujEle(iterator.prawo, rec, g, transparentPink);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < iterator.lista.Count; ++i)
+                {
+                    if (rec.IntersectsWith(iterator.lista[i].wymiary)) iterator.lista[i].Rysuj(g, transparentPink);
+
+                }
+            }
+        }
+        public void RysujMape(Graphics graph, Image przeszkoda_mapa)
+        {
+            RysujEleMapa(root.lewo, graph, przeszkoda_mapa);
+            RysujEleMapa(root.prawo, graph, przeszkoda_mapa);
+        }
+        private void RysujEleMapa(ElementDrzewa<Przeszkoda> iterator, Graphics graph, Image przeszkoda_mapa)
+        {
+            if (iterator.lewo != null || iterator.prawo != null)
+            {
+                if (iterator.lewo != null)
+                {
+                    RysujEleMapa(iterator.lewo, graph, przeszkoda_mapa);
+                }
+                if (iterator.prawo != null)
+                {
+                    RysujEleMapa(iterator.prawo, graph, przeszkoda_mapa);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < iterator.lista.Count; ++i)
+                {
+                    graph.DrawImage(przeszkoda_mapa, iterator.lista[i].wymiary.X / 5 + 20,
+                                iterator.lista[i].wymiary.Y / 5 + 40);
+                }
+            }
+        }
+    }
+}
---
+++ trunk/src/Drzewo.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace Rudy_103.src
+{
+    /// <summary>
+    /// Generyczna klasa drzewa BSP.
+    /// </summary>
+    /// <typeparam name="T">Wstępnie przyjmuje tylko typ "Obiekty"</typeparam>
+    class Drzewo<T> where T : Obiekty
+    {
+        /// <summary>
+        /// Korzeń drzewa.
+        /// </summary>
+        public ElementDrzewa<T> root {get; set;}
+        /// <summary>
+        /// Maksymalna głebokość drzewa. Określa jak dokładne mają być porównania regionów.
+        /// </summary>
+        public int glebokosc { get; set; }
+        /// <summary>
+        /// Konstruktor drzewa BSP
+        /// </summary>
+        /// <param name="lista_obiektow">Lista wszystkich obiektów podlegających podziałowi</param>
+        /// <param name="wymiar">Prostokąt obiektu podlegającego partycjowaniu</param>
+        /// <param name="glebokosc">Określa ile ma być węzłów w drzewie.</param>
+        /// <param name="kompresja">Parametr umożliwiający tworzenie drzewa zkompresowanego(Zawierającego tylko listy w liściach.</param>
+        public Drzewo(List<T> lista_obiektow, Rectangle wymiar, int glebokosc, bool kompresja)
+        {
+            this.glebokosc = glebokosc;
+            if (kompresja)
+            {
+                root = new ElementDrzewa<T>(null, wymiar, 0);
+                TworzDrzewo(root, lista_obiektow, true, 0);
+            }
+            else
+            {
+                root = new ElementDrzewa<T>(lista_obiektow, wymiar, 0);
+                TworzDrzewo(root, true, 0);
+            }
+        }
+        /// <summary>
+        /// Metoda wewnętrzna tworząca "zwykłe" drzewo. 
+        /// </summary>
+        /// <param name="iterator">Domyślnie korzeń</param>
+        /// <param name="dzielXY">Parametr okraślający jak dzielić obszar</param>
+        /// <param name="iteracja">Liczba wykonań metody</param>
+        private void TworzDrzewo(ElementDrzewa<T> iterator, bool dzielXY, int iteracja)
+        {
+            if (iteracja < glebokosc)
+            {
+                iteracja++;
+                Rectangle k1, k2;
+                List<T> lista1 = new List<T>();
+                List<T> lista2 = new List<T>();
+                if (dzielXY)
+                {
+                    k1 = new Rectangle(iterator.Wymiary.X, iterator.Wymiary.Y, iterator.Wymiary.Width / 2, iterator.Wymiary.Height);
+                    k2 = new Rectangle(iterator.Wymiary.X + iterator.Wymiary.Width / 2, iterator.Wymiary.Y, iterator.Wymiary.Width / 2, iterator.Wymiary.Height);
+                }
+                else
+                {
+                    k1 = new Rectangle(iterator.Wymiary.X, iterator.Wymiary.Y, iterator.Wymiary.Width, iterator.Wymiary.Height / 2);
+                    k2 = new Rectangle(iterator.Wymiary.X, iterator.Wymiary.Y + iterator.Wymiary.Height / 2, iterator.Wymiary.Width, iterator.Wymiary.Height / 2);
+                }
+                for (int i = 0; i < iterator.lista.Count; ++i)
+                {
+                    if (k1.Contains(iterator.lista[i].wymiary))
+                    {
+                        lista1.Add(iterator.lista[i]);
+                    }
+                    if (k2.Contains(iterator.lista[i].wymiary))
+                    {
+                        lista2.Add(iterator.lista[i]);
+                    }
+                }
+                if (lista1.Count != 0)
+                {
+                    iterator.lewo = new ElementDrzewa<T>(lista1, k1, iteracja);
+                    TworzDrzewo(iterator.lewo, !dzielXY, iteracja);
+                }
+                if(lista2.Count != 0)
+                {
+                    iterator.prawo = new ElementDrzewa<T>(lista2, k2, iteracja);
+                    TworzDrzewo(iterator.prawo, !dzielXY, iteracja);
+                }
+            }
+        }
+        /// <summary>
+        /// Metoda wewnętrzna tworząca "zkompresowane" drzewo. 
+        /// </summary>
+        /// <param name="iterator">Domyślnie korzeń</param>
+        /// <param name="lista_obiektow">Lista obiektów podlegająca podziałowi w bierzącej iteracji</param>
+        /// <param name="dzielXY">Parametr okraślający jak dzielić obszar</param>
+        /// <param name="iteracja">Liczba wykonań metody</param>
+        private void TworzDrzewo(ElementDrzewa<T> iterator, List<T> lista_obiektow, bool dzielXY, int iteracja)
+        {
+            if (iteracja < glebokosc)
+            {
+                iteracja++;
+                Rectangle k1, k2;
+                List<T> lista1 = new List<T>();
+                List<T> lista2 = new List<T>();
+                if (dzielXY)
+                {
+                    k1 = new Rectangle(iterator.Wymiary.X, iterator.Wymiary.Y, iterator.Wymiary.Width / 2, iterator.Wymiary.Height);
+                    k2 = new Rectangle(iterator.Wymiary.X + iterator.Wymiary.Width / 2, iterator.Wymiary.Y, iterator.Wymiary.Width / 2, iterator.Wymiary.Height);
+                }
+                else
+                {
+                    k1 = new Rectangle(iterator.Wymiary.X, iterator.Wymiary.Y, iterator.Wymiary.Width, iterator.Wymiary.Height / 2);
+                    k2 = new Rectangle(iterator.Wymiary.X, iterator.Wymiary.Y + iterator.Wymiary.Height / 2, iterator.Wymiary.Width, iterator.Wymiary.Height / 2);
+                }
+                for (int i = 0; i < lista_obiektow.Count; ++i)
+                {
+                    if (k1.Contains(lista_obiektow[i].wymiary))
+                    {
+                        lista1.Add(lista_obiektow[i]);
+                    }
+                    if (k2.Contains(lista_obiektow[i].wymiary))
+                    {
+                        lista2.Add(lista_obiektow[i]);
+                    }
+                }
+                if (iteracja == glebokosc)
+                {
+                    if (lista1.Count != 0)
+                    {
+                        iterator.lewo = new ElementDrzewa<T>(lista1, k1);
+                        TworzDrzewo(iterator.lewo, lista1, !dzielXY, iteracja);
+                    }
+                    if (lista2.Count != 0)
+                    {
+                        iterator.prawo = new ElementDrzewa<T>(lista2, k2);
+                        TworzDrzewo(iterator.prawo, lista2, !dzielXY, iteracja);
+                    }
+                }
+                else
+                {
+                    if (lista1.Count != 0)
+                    {
+                        iterator.lewo = new ElementDrzewa<T>(null, k1);
+                        TworzDrzewo(iterator.lewo, lista1, !dzielXY, iteracja);
+                    }
+                    if (lista2.Count != 0)
+                    {
+                        iterator.prawo = new ElementDrzewa<T>(null, k2);
+                        TworzDrzewo(iterator.prawo, lista2, !dzielXY, iteracja);
+                    }
+                }
+            }
+        }
+    }
+}
